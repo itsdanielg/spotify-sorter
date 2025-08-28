cb4a853f906971aba5aed9832cc83bef445ec0b2
itsdanielg <danielg_97@yahoo.com>
2025-08-27 23:40:42 -0400

    tailwind v4 changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [react(), tsConfigPaths()],
-  server: {
-    port: 3000,
-    strictPort: true,
-    host: true
-  },
-  preview: {
-    port: 8080,
-    strictPort: true,
-    host: true
-  }
+  plugins: [react(), tsConfigPaths(), tailwindcss()],
+  server: { port: 3000, strictPort: true, host: true },
+  preview: { port: 8080, strictPort: true, host: true }
 });
