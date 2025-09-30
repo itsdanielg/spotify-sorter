02c42a004c393797e20bbd33bd51dda688f1c9f8
itsdanielg <danielg_97@yahoo.com>
2025-09-30 17:09:03 -0400

    Migrated themes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      animation: { fade: "fadeOut 0.5s 2.6s", linkFade: "fadeIn 0.25s", snackbar: "fadeIn 0.5s, fadeOut 0.5s 4.6s" },
-      keyframes: () => ({
-        fadeIn: { "0%": { opacity: 0 }, "100%": { opacity: 1 } },
-        fadeOut: { "0%": { opacity: 1 }, "100%": { opacity: 0 } }
-      }),
-      fontSize: { xxs: "0.625rem" }
-    }
-  },
   plugins: []
 };
